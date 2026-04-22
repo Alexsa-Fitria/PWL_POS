4f379d19d1dfb0c4c758e70da45cd98fa8fe5121
alexsa <lexafitriayu@gmail.com>
2026-04-23 06:18:22 +0700

    Praktikum 2.7: Membuat relasi One to Many antara User dan Level

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -11,16 +11,14 @@
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th>
-            <th>Aksi</th>
+            <th>Level Pengguna</th>
         </tr>
         @foreach ($data as $d)
         <tr>
             <td>{{ $d->user_id }}</td>
             <td>{{ $d->username }}</td>
             <td>{{ $d->nama }}</td>
-            <td>{{ $d->level_id }}</td>
-            <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
+            <td>{{ $d->level->level_nama ?? '-' }}</td>
         </tr>
         @endforeach
     </table>
