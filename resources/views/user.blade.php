293da3bb802dc85160d714d0b678a6120ec64133
alexsa <lexafitriayu@gmail.com>
2026-04-23 03:53:05 +0700

    Praktikum 2.1: Mencoba find(), where()->first(), firstWhere(), findOr(), dan findOrFail() pada Eloquent ORM

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,24 +6,18 @@
 <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Name</th>
-                <th>ID Level Pengguna</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($data as $d)
-            <tr>
-                <td>{{ $d->user_id }}</td>
-                <td>{{ $d->username }}</td>
-                <td>{{ $d->nama }}</td>
-                <td>{{ $d->level_id }}</td>
-            </tr>
-            @endforeach
-        </tbody>
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Name</th>
+            <th>ID Level Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
+        </tr>
     </table>
 </body>
 </html>
